b00049a4993ae115d837bab045877e538785071c
ArneshRC <84434238+ArneshRC@users.noreply.github.com>
2022-10-23 22:06:57 +0530

    basic static server implementation

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
+use std::path::{Path, PathBuf};
+use rocket::fs::NamedFile;
+
 #[macro_use] extern crate rocket;
 
+#[get("/<file..>")]
+async fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(
+        Path::new("./frontend/build/").join(file)
+    ).await.ok()
+}
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+async fn index() -> Option<NamedFile> {
+    NamedFile::open(
+        Path::new("./frontend/build/index.html")
+    ).await.ok()
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, files])
 }
 
