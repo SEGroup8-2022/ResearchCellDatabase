ca4075be1d99f30b7090173a0dc1d4e8ea1254d7
ArneshRC <84434238+ArneshRC@users.noreply.github.com>
2022-10-24 11:04:14 +0530

    barely functional server implemented

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,53 @@
-use std::path::{Path, PathBuf};
-use rocket::fs::NamedFile;
+use rocket::response::content::{RawHtml, RawJavaScript, RawJson};
+use rocket::response::Responder;
+
+use rocket::serde::json;
 
 #[macro_use] extern crate rocket;
 
-#[get("/<file..>")]
-async fn files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(
-        Path::new("./frontend/build/").join(file)
-    ).await.ok()
-}
+static INDEX_HTML: &str = include_str!("./frontend/build/index.html");
+static MAIN_JS: &str = include_str!("./frontend/build/main.js");
+static FAVICON_ICO: &[u8] = include_bytes!("./frontend/build/favicon.ico");
+
+#[derive(Responder)]
+#[response(status = 200, content_type = "image/x-icon")]
+struct Favicon(&'static [u8]);
 
 #[get("/")]
-async fn index() -> Option<NamedFile> {
-    NamedFile::open(
-        Path::new("./frontend/build/index.html")
-    ).await.ok()
+fn root_handler() -> RawHtml<&'static str> {
+    RawHtml(INDEX_HTML)
+}
+
+#[get("/index.html")]
+fn index_handler() -> RawHtml<&'static str> {
+    RawHtml(INDEX_HTML)
+}
+
+#[get("/main.js")]
+fn script_handler() -> RawJavaScript<&'static str> {
+    RawJavaScript(MAIN_JS)
+}
+
+#[get("/favicon.ico")]
+fn favicon_handler() -> Favicon {
+    Favicon(FAVICON_ICO)
+}
+
+#[get("/records")]
+fn records_handler() -> RawJson<&'static str> {
+
+    RawJson("[[1235,\"Kunal\",\"kunal@example.com\",\"C++ Application\",\"Tannennbaum\",2020],[1230,\"ARC\",\"arc@example.com\",\"Low Level Audio Processing\",\"Acoustics\",2021]]")
+
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, files])
+    rocket::build().mount("/", routes![
+        root_handler,
+        index_handler,
+        script_handler,
+        favicon_handler,
+        records_handler
+    ])
 }
 
